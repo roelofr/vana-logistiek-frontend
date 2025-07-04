384ec4e76c4463ae991dd79fa1787bd84d0af91a
Roelof Roos <github@roelofr.dev>
2025-07-04 21:14:16 +0200

    fix: posts as unused

--- app/(dashboard)/tickets/page.tsx
+++ app/(dashboard)/tickets/page.tsx
@@ -1,4 +1,5 @@
-'use client';
+'use server';
+
 import * as React from 'react';
 import {PageContainer} from '@toolpad/core/PageContainer';
 import CustomDataGrid from '../../components/CustomDataGrid';
@@ -7,6 +8,7 @@ import {columns, rows} from '../../mocks/gridOrdersData';
 export default async function TicketPage() {
     const data = await fetch('https://api.vercel.app/blog')
     const posts = await data.json()
+    console.log('Posts is %o', posts)
     return (
         <PageContainer>
             <CustomDataGrid rows={rows} columns={columns}/>
