c34d7e9dda7931f897b9f454e214451211bb7c5d
Roelof Roos <github@roelofr.dev>
2025-07-13 20:12:12 +0200

    fix: rollback middleware to work again

--- middleware.ts
+++ middleware.ts
@@ -1,7 +1,6 @@
 export {auth as middleware} from '@/auth'
 
 export const config = {
-    matcher: [
-        '/^((?!auth|api|_next/static|_next/image|.*\\.png$).*)'
-    ],
+    // https://nextjs.org/docs/app/building-your-application/routing/middleware#matcher
+    matcher: ['/((?!api|_next/static|_next/image|.*\\.png$).*)'],
 };
