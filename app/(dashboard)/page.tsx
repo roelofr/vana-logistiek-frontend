edd0155c93e4df0cc9f4c10e7ba2720c415b6065
Roelof Roos <github@roelofr.dev>
2025-07-21 20:02:16 +0200

    fix: remove homepage speed dial

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,6 +1,5 @@
 import * as React from 'react';
 import {PageContainer} from '@toolpad/core/PageContainer';
-import AppSpeedDial from "@/app/components/AppSpeedDial";
 import Typography from "@mui/material/Typography";
 import Divider from '@mui/material/Divider';
 
@@ -33,8 +32,6 @@ export default function Dashboard() {
                 Kusjes,<br/>
                 Roelof
             </Typography>
-
-            <AppSpeedDial/>
         </PageContainer>
     );
 }
