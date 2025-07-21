1633e0e41cb5a3baf3cf1c29a20f07d970cd417a
Roelof Roos <github@roelofr.dev>
2025-07-21 19:47:38 +0200

    feat: remove dashboard

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,14 +1,17 @@
 import * as React from 'react';
 import {PageContainer} from '@toolpad/core/PageContainer';
-import DashboardContent from './DashboardContent';
 import AppSpeedDial from "@/app/components/AppSpeedDial";
+import Typography from "@mui/material/Typography";
 
 export default function Dashboard() {
     return (
         <PageContainer>
-            <DashboardContent/>
+            <Typography variant="body1">
+                Hey, welkom in de Logistiek-app. Als je net een account hebt gemaakt, kan je nog niks.<br/>
+                Vraag Tessa om de nodige rechten, dan komt het vast goed.
+            </Typography>
 
-            <AppSpeedDial />
+            <AppSpeedDial/>
         </PageContainer>
     );
 }
