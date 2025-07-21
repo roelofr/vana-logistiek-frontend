10b3231fb43f42123561a164b7677f09169e2f31
Roelof Roos <github@roelofr.dev>
2025-07-21 20:01:47 +0200

    feat: replace Dashboard with text

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -2,13 +2,36 @@ import * as React from 'react';
 import {PageContainer} from '@toolpad/core/PageContainer';
 import AppSpeedDial from "@/app/components/AppSpeedDial";
 import Typography from "@mui/material/Typography";
+import Divider from '@mui/material/Divider';
 
 export default function Dashboard() {
     return (
-        <PageContainer>
+        <PageContainer title="LogistiekApp">
             <Typography variant="body1">
-                Hey, welkom in de Logistiek-app. Als je net een account hebt gemaakt, kan je nog niks.<br/>
-                Vraag Tessa om de nodige rechten, dan komt het vast goed.
+                Beste lezer, welkom.<br/>
+                <br/>
+                Dit is een app voor het <strong>beste</strong> team van de opbouw.<br/>
+                Hoor je niet bij Logistiek? Dan zie je niet zo heel erg veel op deze site.<br/>
+                Maar niet getreurd; wij wel!<br/>
+                <br/>
+                Rechtsbovenin zit de confettiknop. Ik hoop dat je er bij van wordt.<br/>
+                <br/>
+                Beste logistieker,<br/>
+                <br/>
+                Je bent fantastisch bezig, ik houd van jullie allemaal, ga zo Door en vergeet niet te eten.<br/>
+                <br/>
+                Kusjes,<br/>
+                Tessa
+            </Typography>
+
+            <Divider variant="middle" sx={{my: 4}}/>
+
+            <Typography>
+                Heb je hier niets te zoeken, maar nu wel een account? Log dan uit.<br/>
+                De gegevens achter deze applicatie, inclusief alle accounts, worden per 1 september gewist.<br/>
+                <br/>
+                Kusjes,<br/>
+                Roelof
             </Typography>
 
             <AppSpeedDial/>
