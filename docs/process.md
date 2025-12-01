dfe3765e61f43a70d5e5b90688f7ce6478657c5d
Roelof Roos <github@roelofr.dev>
2025-12-01 18:43:34 +0100

    feat: tickets api

---
+++ docs/process.md
@@ -0,0 +1,8 @@
+# Processen
+
+Er zijn een paar processen binnen de Logistiek app
+
+## Aanmaken ticket
+
+Een ticket aanmaken gebeurt door het kiezen van een standhouder, het typen van een onderwerp en een omschrijving. Hierna wordt het ticket aangemaakt en overgedragen aan het Team CP.
+
---
+++ server/api/tickets.ts
@@ -0,0 +1,25 @@
+const tickets = [
+  {
+    id: 1,
+    title: 'Bijbestelling',
+    vendor: {
+      id: 1,
+      name: 'Spooky Stuff',
+    },
+    messages: [
+      {
+        id: 222,
+        type: 'message',
+        author: {
+          id: 2,
+          name: 'Roelof',
+          avatar: ''
+        }
+      }
+    ]
+  }
+]
+
+export default eventHandler(async () => {
+  return members
+})
