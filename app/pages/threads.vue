a31a74f9d42159fa892e823b8d33ac5a6bd794af
Roelof Roos <github@roelofr.dev>
2025-12-20 20:59:55 +0100

    feat: Inbox to Thread

--- app/pages/threads.vue
+++ app/pages/threads.vue
@@ -75,10 +75,10 @@ const isMobile = breakpoints.smaller('lg')
         />
       </template>
     </UDashboardNavbar>
-    <InboxList v-model="selectedMail" :threads="filteredMails" :is-loading="pending" />
+    <ThreadList v-model="selectedMail" :threads="filteredMails" :is-loading="pending" />
   </UDashboardPanel>
 
-  <InboxMail v-if="selectedMail" :mail="selectedMail" @close="selectedMail = null" />
+  <ThreadMessage v-if="selectedMail" :mail="selectedMail" @close="selectedMail = null" />
   <div v-else class="hidden lg:flex flex-1 items-center justify-center">
     <UIcon name="i-lucide-inbox" class="size-32 text-dimmed" />
   </div>
@@ -86,7 +86,7 @@ const isMobile = breakpoints.smaller('lg')
   <ClientOnly>
     <USlideover v-if="isMobile" v-model:open="isMailPanelOpen">
       <template #content>
-        <InboxMail v-if="selectedMail" :mail="selectedMail" @close="selectedMail = null" />
+        <ThreadMessage v-if="selectedMail" :mail="selectedMail" @close="selectedMail = null" />
       </template>
     </USlideover>
   </ClientOnly>
